37dd89fab71577c5efe4589d84cc23c10a0e41a2
Luigi <paluigi@gmail.com>
2021-12-15 12:07:47 +0100

    First working example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# flask-htmx
- flask htmx test
+# Flask-Sock and HTMX example
+Very short example to show the integration of [Flask-Sock](https://flask-sock.readthedocs.io/en/latest/) and [HTMX](https://htmx.org/) for self-updating web pages.   
+HTMX files v1.6.1 are copied in the static folder for offline tests or deployments.  
+
+Have fun!  
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask_sock import Sock
+import time
+from datetime import datetime
+import requests
+import json
+
+app = Flask(__name__)
+sock = Sock(app)
+
+api_endpoint = "https://api.publicapis.org/random"
+
+
+@app.route("/index.html")
+def root():
+    return render_template("index.html")
+
+
+@sock.route("/clock")
+def timeflow(ws):
+    while True:
+        new_time = """<div id="clock"><p>Current date and time: {}</p></div>""".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        ws.send(new_time)
+        time.sleep(5)
+
+
+@sock.route("/api")
+def apiflow(ws):
+    while True:
+        new_api = requests.get(api_endpoint)
+        api_dict = json.loads(new_api.text).get("entries")[0]
+        new_text = """<div id="api"><p>API name: {0}</p><p>Description: {1}</p><p>link: <a href={2} target="_blank">{2}</a></p></div>""".format(api_dict.get("API"), api_dict.get("Description"), api_dict.get("Link"))
+        ws.send(new_text)
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
