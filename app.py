16d95e8c8eed76b0b654749a0590761caec3a797
Luigi <paluigi@gmail.com>
2021-12-15 15:28:14 +0100

    updated formatting

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ sock = Sock(app)
 api_endpoint = "https://api.publicapis.org/random"
 
 
+@app.route("/")
 @app.route("/index.html")
 def root():
     return render_template("index.html")
@@ -19,7 +20,9 @@ def root():
 @sock.route("/clock")
 def timeflow(ws):
     while True:
-        new_time = """<div id="clock"><p>Current date and time: {}</p></div>""".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        new_time = """<div id="clock"><p>Current date and time: {}</p></div>""".format(
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        )
         ws.send(new_time)
         time.sleep(5)
 
@@ -29,10 +32,12 @@ def apiflow(ws):
     while True:
         new_api = requests.get(api_endpoint)
         api_dict = json.loads(new_api.text).get("entries")[0]
-        new_text = """<div id="api"><p>API name: {0}</p><p>Description: {1}</p><p>link: <a href={2} target="_blank">{2}</a></p></div>""".format(api_dict.get("API"), api_dict.get("Description"), api_dict.get("Link"))
+        new_text = """<div id="api"><p>API name: {0}</p><p>Description: {1}</p><p>link: <a href={2} target="_blank">{2}</a></p></div>""".format(
+            api_dict.get("API"), api_dict.get("Description"), api_dict.get("Link")
+        )
         ws.send(new_text)
         time.sleep(10)
 
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
